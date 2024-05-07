06bff808ad19adc608d0bd94399c6f4eb05bc364
IsaqjonovII <ilhomjonisoqjonov2@gmail.com>
2024-05-07 19:32:57 +0500

    fix: header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,10 @@
+<script setup>
+    import Logo from '@/components/Logo';
+</script>
 <template>
-    <header>
-        
+    <header class="bg-white p-4">
+        <div class="container flex-between">
+            <Logo />
+        </div>
     </header>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
+<script setup>
+  import Header from '@/components/Header.vue';
+</script>
+
 <template>
   <main>
+    <Header />
     <h1>helloo dudee</h1>
   </main>
 </template>
